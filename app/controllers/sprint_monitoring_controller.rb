95347e2817013d10f57400e410f8d1abaf67a493
Freddy Florenca <freddyflorenca@gmail.com>
2012-01-19 16:50:08 -0430

    graficos en el sprint monitoring y parte del burndown chart

--- app/controllers/sprint_monitoring_controller.rb
+++ app/controllers/sprint_monitoring_controller.rb
@@ -44,16 +44,60 @@ class SprintMonitoringController < ApplicationController
 
       milestone_id = params[:milestone_id]
       iteration = Milestone.find_by_id(milestone_id)
+
+      init_date = Date.civil(iteration.init_date.year,iteration.init_date.month,iteration.init_date.day)
+      due_date = Date.civil(iteration.due_date.year,iteration.due_date.month,iteration.due_date.day)
+      duration_days = (due_date - init_date).to_i
+
+      @burndown_data = Array.new
       @closed_us = Array.new
       @open_us = Array.new
+      @burned_points = 0
+      @remaining_points = 0
+      @added_value = 0
+      @remaining_value = 0      
+
+#      prueba = Date.new(2012,1,1)
+#      if ( dias[0] == prueba)
+#         @dias = 'JOJOJO'
+#      end
+
+#      iteracion = Milestone.find_by_id(8)
+#      dia = Date.new(iteracion.init_date.year,iteracion.init_date.month,iteracion.init_date.day)
+
+
 
       iteration.tasks.each do |us|
         if us.status == 1
           @closed_us << us
+          @burned_points = @burned_points + us.total_points
+          @added_value = @added_value + us.business_value
         end
         if us.status == 0
           @open_us << us
+          @remaining_points = @remaining_points + us.total_points
+          @remaining_value = @remaining_value + us.business_value
         end
+      end
+
+      @burndown_data = Array.new
+
+      dias = Array.new
+      for i in 0..duration_days
+        dias << init_date + i
+      end
+
+      n = 1
+      dias.each do |dia|
+        @burndown_data[n] = dia
+        @burndown_data[n+1] = 0
+        @closed_us.each do |us|
+          completed_at = us.completed_at.to_date
+          if(completed_at == dia)
+            @burndown_data[n+1] = @burndown_data[n+1] + us.total_points
+          end
+        end
+        n = n + 2
       end
 
     end
