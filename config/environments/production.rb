36e2d6c12018663abd20bc07b962c22612eb36f2
Freddy Florenca <freddyflorenca@gmail.com>
2012-04-11 15:29:56 -0430

    cambiado el sistema de correo a smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Jobsworth::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
